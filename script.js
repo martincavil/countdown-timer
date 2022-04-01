b4eb2dedcda362038e751fa00be494f863804af4
Martin Cavil <martin.cavil98@gmail.com>
2022-04-01 10:17:57 +0200

    fixed some issues

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Define the targe
+// Define the target
 const target = '1 jan 2023';
 
 // Select elements from HTML
 const daysElement = document.querySelector('#days');
 const hoursElement = document.querySelector('#hours');
-const minutesElement = document.querySelector('#minutes');
+const minsElement = document.querySelector('#mins');
 const secondsElement = document.querySelector('#seconds');
 
 // Create the function
@@ -20,17 +20,17 @@ function countdown() {
   const totalSeconds = (targetDate - currentDate) / 1000;
   const days = Math.floor(totalSeconds / 3600 / 24);
   const hours = Math.floor(totalSeconds / 3600) % 24;
-  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const mins = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
 
   // Reasign value for HTML elements
   daysElement.innerHTML = days;
   hoursElement.innerHTML = hours;
-  minutesElement.innerHTML = minutes;
+  minsElement.innerHTML = mins;
   secondsElement.innerHTML = seconds;
 
 
-  // console.log(days, hours, minutes, seconds);
+  // console.log(days, hours, mins, seconds);
 }
 
 countdown();
